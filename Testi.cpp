22b052096cfb81cb55dba39512292fc6f84791de
Niki <nikniksham@gmail.com>
2025-01-25 13:47:11 +0300

    4 теста готовы

--- Testi.cpp
+++ Testi.cpp
@@ -7,9 +7,10 @@
 using namespace std;
 
 int main() {
-    bool test1 = true;
-    bool test2 = true;
-    bool test3 = true;
+    bool test1 = false;
+    bool test2 = false;
+    bool test3 = false;
+    bool test4 = true;
     std::chrono::time_point<std::chrono::high_resolution_clock> start;
     chrono::duration<double> duration;
     int fd, times;
@@ -112,6 +113,38 @@ int main() {
         duration = chrono::high_resolution_clock::now() - start;
         cout << "Время выполнения с кэшем: " << duration.count() << " секунд.\n";
 
+
+        free_all_cache_blocks();
+        reset_cache_stats();
+        cout<<"\n----------------------------------------\n\n\n";
+    }
+    if (test4) {
+        char* filename1 = "cifri.txt";  // 6780
+        char* filename2 = "bukvi.txt";  // 5761
+        char* filename3 = "itoito.txt"; // 0
+        char *buf = new char[100];
+
+        cout<<"Тест #4 - Работа с несколькими файлами одновременно\n\n";
+
+        int fd1 = lab2_open(filename1);
+        int fd2 = lab2_open(filename2);
+        int fd3 = lab2_open(filename3);
+
+        lab2_lseek(fd1, get_rand_from_to(0, 6780), 0);
+        lab2_read(fd1, buf, 100);
+
+        lab2_write(fd3, buf, 50);
+
+        lab2_lseek(fd2, get_rand_from_to(0, 5761), 0);
+        lab2_read(fd2, buf, 100);
+
+        lab2_write(fd3, buf, 100);
+
+        lab2_fsync(fd3);
+        lab2_close(fd1);
+        lab2_close(fd2);
+        lab2_close(fd3);
+        cout << "Cache hits: " << get_cache_stats().cache_hits << ", Cache miss: " << get_cache_stats().cache_miss << endl;
 
         free_all_cache_blocks();
         reset_cache_stats();
